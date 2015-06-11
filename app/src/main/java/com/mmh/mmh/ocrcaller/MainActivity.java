e9d6656d73638e4055625db7b14a30c1d7fc71b9
Mahesh Hegde <maheshmhegade@gmail.com>
2015-06-11 12:35:15 +0530

    push ocrcaller to cloud

---
+++ app/src/main/java/com/mmh/mmh/ocrcaller/MainActivity.java
@@ -0,0 +1,175 @@
+package com.mmh.mmh.ocrcaller;
+
+import android.content.Intent;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Uri fileUri;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    public static MainActivity ActivityContext =null;
+    public static TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orc_camera);
+
+        ActivityContext = this;
+
+        Button buttonCameraOn = (Button)findViewById(R.id.onCamera);
+        output = (TextView)findViewById(R.id.results);
+
+        buttonCameraOn.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // create new Intentwith with Standard Intent action that can be
+                // sent to have the camera application capture an video and return it.
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                // create a file to save the video
+                fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
+
+                Log.d("saved to", fileUri.toString());
+                // set the image file name
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                // set the video image quality to high
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+
+                // start the Video Capture Intent
+                startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+
+            }
+        });
+
+
+    }
+
+    private static Uri getOutputMediaFileUri(int type){
+
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static File getOutputMediaFile(int type){
+
+        // Check that the SDCard is mounted
+
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory() + File.separator + Environment.DIRECTORY_PICTURES
+                + File.separator +"MyCameraVideo" );
+
+
+        // Create the storage directory(MyCameraVideo) if it does not exist
+        if (! mediaStorageDir.exists()){
+
+            if (! mediaStorageDir.mkdirs()){
+
+                output.setText("Failed to create directory MyCameraVideo.");
+
+                Toast.makeText(ActivityContext, "Failed to create directory MyCameraVideo.",
+                        Toast.LENGTH_LONG).show();
+
+
+                return null;
+            }
+        }
+
+        // Create a media file name
+
+        // For unique file name appending current timeStamp with file name
+        java.util.Date date= new java.util.Date();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(date.getTime());
+
+        File mediaFile;
+
+        if(type == MEDIA_TYPE_VIDEO) {
+
+            // For unique video file name appending current timeStamp with file name
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG"+ timeStamp + ".png");
+
+        } else {
+            return null;
+        }
+
+        return mediaFile; //mediaFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // After camera screen this code will excuted
+
+        if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+
+            if (resultCode == RESULT_OK) {
+
+                output.setText("Video File : " +data.getData());
+
+                // Video captured and saved to fileUri specified in the Intent
+                Toast.makeText(this, "Video saved to: " +
+                        data.getData(), Toast.LENGTH_LONG).show();
+
+            } else if (resultCode == RESULT_CANCELED) {
+
+                output.setText("User cancelled the video capture.");
+
+                // User cancelled the video capture
+                Toast.makeText(this, "User cancelled the video capture.",
+                        Toast.LENGTH_LONG).show();
+
+            } else {
+
+                output.setText("Video capture failed.");
+
+                // Video capture failed, advise user
+                Toast.makeText(this, "Video capture failed.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
